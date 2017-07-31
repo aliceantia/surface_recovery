694a60f657289979147120231b291fccbb859db1
swellpeaks <xu3369@gmail.com>
2017-07-31 10:37:35 -0400

    invariance

---
+++ klein_bottle/invariance/invariant_qp.m
@@ -0,0 +1,65 @@
+addpath('../../matlab_code/GeometryTools');
+addpath('../../matlab_code/ripser');
+addpath('../../matlab_code/TDETools');
+
+%% Define system
+
+T = 10000;
+
+beta = sqrt(5)/1000;
+numXPeriods = 100;
+alpha = sqrt(2)*beta*numXPeriods;
+tMax = 2*pi/beta;
+t = linspace(0, tMax, T);
+
+f = cos(alpha*t) + cos(beta*t);
+
+%% observation function
+
+dim = 20;
+Tau = 1;
+dT = 1;
+
+%% dot product
+
+X = getSlidingWindow(f, dim, Tau, dT);
+
+Y = getPCA(X);
+X = getGreedyPerm(X, 300);
+
+%% Compute PH of the embedding
+
+disp('Doing TDA (mod 2)...');
+DX = getSSM(X);
+IsSliding2 = ripserDM(DX, 2, 2);
+disp('Doing TDA (mod 3)...');
+IsSliding3 = ripserDM(DX, 3, 2);
+
+figure(1);
+clf;
+
+subplot(321);
+C = plotTimeColors(1:length(f), f, 'type', '2DLine');
+title('SSM Original');
+
+subplot(322);
+Y = Y(:, 1:3); %Go down to 3D PCA
+C = C(1:size(Y, 1), :);
+scatter3(Y(:, 1), Y(:, 2), Y(:, 3), 20, C(:, 1:3), 'fill');
+title('PCA Phi');
+
+subplot(323);
+plotDGM(IsSliding2{2});
+title('H1 Phi (mod 2)');
+
+subplot(324);
+plotDGM(IsSliding2{3});
+title('H2 Phi (mod 2)');
+
+subplot(325);
+plotDGM(IsSliding3{2});
+title('H1 Phi (mod 3)');
+
+subplot(326);
+plotDGM(IsSliding3{3});
+title('H2 Phi (mod 3)');
