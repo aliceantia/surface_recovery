7eb23309e5203b7748adc267ac7643d3fde3fbae
aliceantia <antiaa@carleton.edu>
2017-08-01 12:12:40 -0400

    fixed get distance matrix

--- klein_bottle/tests/window_size.m
+++ klein_bottle/tests/window_size.m
@@ -1,8 +1,8 @@
-addpath('../matlab_code/GeometryTools');
-addpath('../matlab_code/ripser');
-addpath('../matlab_code/TDETools');
-addpath('../matlab_code/samirFunctions');
-addpath('../sw_distortion');
+addpath('../../matlab_code/GeometryTools');
+addpath('../../matlab_code/ripser');
+addpath('../../matlab_code/TDETools');
+addpath('../../matlab_code/samirFunctions');
+addpath('../../sw_distortion');
 
 
 %% Define trajectory
@@ -16,13 +16,15 @@ thetas = linspace(0, scale*numPeriods, numIterations);
 phis = linspace(0, scale*height, numIterations);
 
 
-%% distance on klein
+%% distance on torus and klein
+%  L1
 dTorusL1 = @(p1, p2) ...
     min(abs(p1(1) - p2(1)), scale - abs(p1(1) - p2(1))) ...
     + min(abs(p1(2) - p2(2)), scale - abs(p1(2) - p2(2)));
 
 dKleinL1 = @(p1, p2) min(dTorusL1(p1, p2),...
     dTorusL1(p1, [mod(p2(1)+scale/2, scale) mod(-p2(2), scale)]));
+% L2 
 dTorusL2 = @(p1,p2) ...
     sqrt(min(abs(p1(1) - p2(1)), scale - abs(p1(1) - p2(1))).^2 ...
     + min(abs(p1(2) - p2(2)), scale - abs(p1(2) - p2(2))).^2);
@@ -36,47 +38,65 @@ phi0 = 0.3*height*scale;
 
 g = @(theta, phi) dTorusL2([theta phi], [theta0 phi0]);
 
-x=zeros(1,numIterations);
+ts=zeros(1,numIterations);
+
 for ii=1:numIterations
-    x(ii) = g(mod(thetas(ii), scale), mod(phis(ii), scale));
+    ts(ii) = g(mod(thetas(ii), scale), mod(phis(ii), scale));
 end
 
 dim = 2*b;
 Tau = 4;
 dT = 1;
-X = getSlidingWindow(x, dim, Tau, dT);
+SW = getSlidingWindow(x, dim, Tau, dT);
 
 Y = getPCA(X);
-X = getGreedyPerm(X, 300);
-%%
-disp('Doing TDA (mod 2)...');
-DX = getSSM(X);
-IsSliding2 = ripserDM(DX, 2, 2);
-disp('Doing TDA (mod 3)...');
-IsSliding3 = ripserDM(DX, 3, 2);
+%% Distortion
+[SWd, Md] = getDistanceMatrix([mod(thetas, scale) mod(phis, scale)], SW, 'dKleinL1');
+toc
+tic
+computeDistortion(Md,SWd)
+toc
+SWd = unrollDistMat(SWd);
+Md = unrollDistMat(Md);
 
+D = SWd./Md;
+%% do TDA
+SW = getGreedyPerm(SW, 300);
+disp('Doing TDA (mod 2)...');
+DSW = getSSM(SW);
+IsSliding2 = ripserDM(DSW, 2, 2);
+disp('Doing TDA (mod 3)...');
+IsSliding3 = ripserDM(DSW, 3, 2);
+
+%% Plots
 clf;
 
-subplot(221);
-C = plotTimeColors(1:length(x), x, 'type', '2DLine');
+subplot(321);
+C = plotTimeColors(1:length(ts), ts, 'type', '2DLine');
 title('SSM Original');
 
-subplot(222);
+subplot(322);
 Y = Y(:, 1:3); %Go down to 3D PCA
 C = C(1:size(Y, 1), :);
 scatter3(Y(:, 1), Y(:, 2), Y(:, 3), 20, C(:, 1:3), 'fill');
 title('PCA Phi');
 
-subplot(223);
+subplot(323);
 plotDGM(IsSliding2{2});
 plotDGM(IsSliding2{3}, 'red', 'd');
 title('Z2 Coefficients');
 
 
-subplot(224);
+subplot(324);
 plotDGM(IsSliding3{2});
 plotDGM(IsSliding3{3}, 'red', 'd');
 title('Z3 Coefficients');
+
+subplot(325)
+scatter(Md,Swd, 2, 'filled');
+title('Distortion');
+xlabel('Distance on original surface');
+ylabel('Distance in SW');
 
 %% Notes
 %{
