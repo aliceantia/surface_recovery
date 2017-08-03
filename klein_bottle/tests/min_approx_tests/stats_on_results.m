a7fceb74a5530c9ef2f5f14a015d441d357286fe
aliceantia <antiaa@carleton.edu>
2017-08-03 10:32:55 -0400

    add file to get distortion and klein persistance from results

---
+++ klein_bottle/tests/min_approx_tests/stats_on_results.m
@@ -0,0 +1,40 @@
+addpath('../../../matlab_code/GeometryTools');
+addpath('../../../matlab_code/ripser');
+addpath('../../../matlab_code/TDETools');
+addpath('../../../matlab_code/samirFunctions');
+addpath('../../../sw_distortion');
+addpath('..');
+
+
+%load dataset before running this
+
+
+%parameters in run
+a = -0.1:0.05:1.2;
+
+kleinPersistence = zeros(length(a), 1);
+distortion = zeros(length(a), 1);
+
+for ii=1:length(a) %length of run
+ii
+
+%get klein persistence for results
+IsSliding2 = softMinApproxResults1{4, ii};
+IsSliding3 = softMinApproxResults1{5, ii};
+
+kleinPersistence(ii) = kleinBottlePersistence(IsSliding2{2}, IsSliding2{3}, IsSliding3{3});
+
+%compute distortion for results
+SWd = softMinApproxResults1{2, ii};
+Md = softMinApproxResults1{3, ii};
+
+distortion(ii) = computeDistortion(Md, SWd);
+end
+
+subplot(1,2,1)
+plot(a', kleinPersistence);
+ylabel('klein persistence');
+
+subplot(122)
+plot(a, distortion);
+ylabel('distortion');
